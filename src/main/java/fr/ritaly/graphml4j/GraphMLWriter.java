9a3b1090f2695e18e10ac3137b53aa16af9d4720
Francois Ritaly <francois.ritaly@gmail.com>
2014-10-21 13:46:14 +0200

    Generate the yEd-specific attributes

--- src/main/java/fr/ritaly/graphml4j/GraphMLWriter.java
+++ src/main/java/fr/ritaly/graphml4j/GraphMLWriter.java
@@ -47,6 +47,71 @@ public class GraphMLWriter {
 			this.streamWriter.writeNamespace("yed", "http://www.yworks.com/xml/yed/3");
 			this.streamWriter.writeAttribute("xsi:schemaLocation", "http://graphml.graphdrawing.org/xmlns http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd");
 
+			// Add the yEd-specific metadata
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("for", "graphml");
+			this.streamWriter.writeAttribute("id", "d0");
+			this.streamWriter.writeAttribute("yfiles.type", "resources");
+
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("for", "port");
+			this.streamWriter.writeAttribute("id", "d1");
+			this.streamWriter.writeAttribute("yfiles.type", "portgraphics");
+
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("for", "port");
+			this.streamWriter.writeAttribute("id", "d2");
+			this.streamWriter.writeAttribute("yfiles.type", "portgeometry");
+
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("for", "port");
+			this.streamWriter.writeAttribute("id", "d3");
+			this.streamWriter.writeAttribute("yfiles.type", "portuserdata");
+
+			// Define the attributes for type 'node'
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("attr.name", "url");
+			this.streamWriter.writeAttribute("attr.type", "string");
+			this.streamWriter.writeAttribute("for", "node");
+			this.streamWriter.writeAttribute("id", "d4");
+
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("attr.name", "description");
+			this.streamWriter.writeAttribute("attr.type", "string");
+			this.streamWriter.writeAttribute("for", "node");
+			this.streamWriter.writeAttribute("id", "d5");
+
+			// Define the type 'node'
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("for", "node");
+			this.streamWriter.writeAttribute("id", "d6");
+			this.streamWriter.writeAttribute("yfiles.type", "nodegraphics");
+
+			// Define the attributes for type 'graph'
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("attr.name", "Description");
+			this.streamWriter.writeAttribute("attr.type", "string");
+			this.streamWriter.writeAttribute("for", "graph");
+			this.streamWriter.writeAttribute("id", "d7");
+
+			// Define the attributes for type 'edge'
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("attr.name", "url");
+			this.streamWriter.writeAttribute("attr.type", "string");
+			this.streamWriter.writeAttribute("for", "edge");
+			this.streamWriter.writeAttribute("id", "d8");
+
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("attr.name", "description");
+			this.streamWriter.writeAttribute("attr.type", "string");
+			this.streamWriter.writeAttribute("for", "edge");
+			this.streamWriter.writeAttribute("id", "d9");
+
+			// Define the type 'edge'
+			this.streamWriter.writeEmptyElement("key");
+			this.streamWriter.writeAttribute("for", "edge");
+			this.streamWriter.writeAttribute("id", "d10");
+			this.streamWriter.writeAttribute("yfiles.type", "edgegraphics");
 		} catch (XMLStreamException e) {
 			throw new GraphMLException(e);
 		}
