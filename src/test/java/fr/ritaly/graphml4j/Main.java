ed0e16b479e5b66fe64d316f84904a0d1e2f62d4
Francois Ritaly <francois.ritaly@gmail.com>
2014-10-22 14:03:11 +0200

    Enhanced sample code

--- src/test/java/fr/ritaly/graphml4j/Main.java
+++ src/test/java/fr/ritaly/graphml4j/Main.java
@@ -8,6 +8,31 @@ public class Main {
 		final StringWriter stringWriter = new StringWriter();
 
 		GraphMLWriter writer = new GraphMLWriter(stringWriter);
+
+		// Customize the rendering
+		final NodeStyle nodeStyle = writer.getNodeStyle();
+		nodeStyle.setBorderType(LineType.LINE);
+		nodeStyle.setBorderWidth(2.0f);
+		nodeStyle.setFillColor("#CCCCCC");
+		nodeStyle.setFontSize(13);
+		nodeStyle.setHeight(60.0f);
+		nodeStyle.setWidth(60.0f);
+		nodeStyle.setShape(Shape.ROUNDED_RECTANGLE);
+		nodeStyle.setTextAlignment(Alignment.CENTER);
+
+		writer.setNodeStyle(nodeStyle);
+
+		final EdgeStyle edgeStyle = writer.getEdgeStyle();
+		edgeStyle.setColor("#000000");
+		edgeStyle.setSmoothed(true);
+		edgeStyle.setSourceArrow(Arrow.CIRCLE);
+		edgeStyle.setTargetArrow(Arrow.DELTA);
+		edgeStyle.setType(LineType.LINE);
+		edgeStyle.setWidth(2.0f);
+
+		writer.setEdgeStyle(edgeStyle);
+
+		// Generate the graph
 		writer.graph();
 		writer.group("TEST");
 
