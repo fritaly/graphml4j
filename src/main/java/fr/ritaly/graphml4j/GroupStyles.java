5a81bdcee4fc3fb5e656bc7c5d626d47d84f99a2
Francois Ritaly <francois.ritaly@gmail.com>
2014-10-23 14:38:55 +0200

    Added setters to class fr.ritaly.graphml4j.GroupStyles

--- src/main/java/fr/ritaly/graphml4j/GroupStyles.java
+++ src/main/java/fr/ritaly/graphml4j/GroupStyles.java
@@ -1,6 +1,16 @@
 package fr.ritaly.graphml4j;
 
+import java.awt.Color;
+
 import org.apache.commons.lang.Validate;
+
+import fr.ritaly.graphml4j.base.Alignment;
+import fr.ritaly.graphml4j.base.FontStyle;
+import fr.ritaly.graphml4j.base.LineType;
+import fr.ritaly.graphml4j.base.Placement;
+import fr.ritaly.graphml4j.base.Position;
+import fr.ritaly.graphml4j.base.Shape;
+import fr.ritaly.graphml4j.base.SizePolicy;
 
 /**
  * The styles applied to a group node. A group can be in 2 states (open or
@@ -56,5 +66,155 @@ public class GroupStyles {
 
 		// Defensive recopy
 		this.openStyle.apply(style);
+	}
+
+	public void setShape(Shape shape) {
+		closedStyle.setShape(shape);
+		openStyle.setShape(shape);
+	}
+
+	public void setShadowColor(Color color) {
+		closedStyle.setShadowColor(color);
+		openStyle.setShadowColor(color);
+	}
+
+	public void setInsets(float value) {
+		closedStyle.setInsets(value);
+		openStyle.setInsets(value);
+	}
+
+	public void setShadowOffsetX(int value) {
+		closedStyle.setShadowOffsetX(value);
+		openStyle.setShadowOffsetX(value);
+	}
+
+	public void setShadowOffsetY(int value) {
+		closedStyle.setShadowOffsetY(value);
+		openStyle.setShadowOffsetY(value);
+	}
+
+	public void setHeight(float height) {
+		closedStyle.setHeight(height);
+		openStyle.setHeight(height);
+	}
+
+	public void setWidth(float width) {
+		closedStyle.setWidth(width);
+		openStyle.setWidth(width);
+	}
+
+	public void setFillColor(Color color) {
+		closedStyle.setFillColor(color);
+		openStyle.setFillColor(color);
+	}
+
+	public void setFillColor2(Color fillColor2) {
+		closedStyle.setFillColor2(fillColor2);
+		openStyle.setFillColor2(fillColor2);
+	}
+
+	public void setBorderColor(Color color) {
+		closedStyle.setBorderColor(color);
+		openStyle.setBorderColor(color);
+	}
+
+	public void setBorderType(LineType borderType) {
+		closedStyle.setBorderType(borderType);
+		openStyle.setBorderType(borderType);
+	}
+
+	public void setTransparentFill(boolean transparentFill) {
+		closedStyle.setTransparentFill(transparentFill);
+		openStyle.setTransparentFill(transparentFill);
+	}
+
+	public void setBorderWidth(float borderWidth) {
+		closedStyle.setBorderWidth(borderWidth);
+		openStyle.setBorderWidth(borderWidth);
+	}
+
+	public void setUnderlinedText(boolean value) {
+		closedStyle.setUnderlinedText(value);
+		openStyle.setUnderlinedText(value);
+	}
+
+	public void setVisible(boolean visible) {
+		closedStyle.setVisible(visible);
+		openStyle.setVisible(visible);
+	}
+
+	public void setTextColor(Color color) {
+		closedStyle.setTextColor(color);
+		openStyle.setTextColor(color);
+	}
+
+	public void setFontSize(int fontSize) {
+		closedStyle.setFontSize(fontSize);
+		openStyle.setFontSize(fontSize);
+	}
+
+	public void setFontFamily(String fontFamily) {
+		closedStyle.setFontFamily(fontFamily);
+		openStyle.setFontFamily(fontFamily);
+	}
+
+	public void setTextAlignment(Alignment textAlignment) {
+		closedStyle.setTextAlignment(textAlignment);
+		openStyle.setTextAlignment(textAlignment);
+	}
+
+	public void setFontStyle(FontStyle fontStyle) {
+		closedStyle.setFontStyle(fontStyle);
+		openStyle.setFontStyle(fontStyle);
+	}
+
+	public void setBackgroundColor(Color backgroundColor) {
+		closedStyle.setBackgroundColor(backgroundColor);
+		openStyle.setBackgroundColor(backgroundColor);
+	}
+
+	public void setLineColor(Color lineColor) {
+		closedStyle.setLineColor(lineColor);
+		openStyle.setLineColor(lineColor);
+	}
+
+	public void setPlacement(Placement placement) {
+		closedStyle.setPlacement(placement);
+		openStyle.setPlacement(placement);
+	}
+
+	public void setPosition(Position position) {
+		closedStyle.setPosition(position);
+		openStyle.setPosition(position);
+	}
+
+	public void setBottomInset(int bottomInset) {
+		closedStyle.setBottomInset(bottomInset);
+		openStyle.setBottomInset(bottomInset);
+	}
+
+	public void setLeftInset(int leftInset) {
+		closedStyle.setLeftInset(leftInset);
+		openStyle.setLeftInset(leftInset);
+	}
+
+	public void setRightInset(int rightInset) {
+		closedStyle.setRightInset(rightInset);
+		openStyle.setRightInset(rightInset);
+	}
+
+	public void setTopInset(int topInset) {
+		closedStyle.setTopInset(topInset);
+		openStyle.setTopInset(topInset);
+	}
+
+	public void setInsets(int value) {
+		closedStyle.setInsets(value);
+		openStyle.setInsets(value);
+	}
+
+	public void setSizePolicy(SizePolicy policy) {
+		closedStyle.setSizePolicy(policy);
+		openStyle.setSizePolicy(policy);
 	}
 }
